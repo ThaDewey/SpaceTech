2d59e95f64205fd321aa4ac0f17c486acda470da
ThaDewey <thadewey@gmail.com>
2023-11-14 00:20:59 -0600

    added odin inspector

--- Assets/WallStud/Inventory/Item.cs
+++ Assets/WallStud/Inventory/Item.cs
@@ -1,6 +1,9 @@
 ﻿using System.ComponentModel;
 using UnityEngine;
 
+/// <summary>
+/// i do not know...we should probably update this
+/// </summary>
 [CreateAssetMenu(fileName = "Item", menuName = "SpaceTech/Inventory/Item", order = 1)]
 public class Item : ScriptableObject {
 
@@ -8,7 +11,4 @@ public class Item : ScriptableObject {
 	public int itemId;
 	public ItemType itemType;
 
-}
-
-public class ItemType {
 }
--- Assets/WallStud/Inventory/ItemData.cs
+++ Assets/WallStud/Inventory/ItemData.cs
@@ -1,6 +1,11 @@
 ﻿using System.ComponentModel;
 using UnityEngine;
 
+
+
+/// <summary>
+/// Item data is a data container for actual items in the game
+/// </summary>
 [CreateAssetMenu(fileName = "Item Data", menuName = "SpaceTech/Inventory/ItemData", order = 1)]
 public class ItemData : ScriptableObject {
 
