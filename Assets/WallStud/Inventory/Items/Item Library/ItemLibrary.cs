f7f28c1bf824b7f37855778d1159fa1c38a189ac
ThaDewey <thadewey@gmail.com>
2023-11-19 10:13:58 -0600

    inentory updates.

---
+++ Assets/WallStud/Inventory/Items/Item Library/ItemLibrary.cs
@@ -0,0 +1,42 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Item data is a data container for actual items in the game
+/// </summary>
+[CreateAssetMenu(fileName = "Item Library", menuName = "SpaceTech/Inventory/Item Library ", order = 1)]
+public class ItemLibrary : ScriptableObject {
+
+	public List<Item> itemLibrary = new List<Item>();
+
+	[Button]
+	private void OrganizeLibrary() {
+
+		foreach (Item item in itemLibrary) {
+			int index = itemLibrary.IndexOf(item);
+			item.itemId = index;
+		}
+	}
+
+	private Item RandomItem() {
+		int index = Random.Range(0, itemLibrary.Count);
+		return itemLibrary[index];
+	}
+
+	public List<Item> PopulateContainer(int amountRequested) {
+		List<Item> container = new List<Item>();
+
+		for (int i = 0; i < amountRequested; i++) {
+			container.Add(RandomItem());
+		}
+
+		return container;
+	}
+
+
+}
--- Assets/WallStud/Scripts/Containers.cs
+++ Assets/WallStud/Scripts/Containers.cs
@@ -1,14 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using JetBrains.Annotations;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Containers : MonoBehaviour, IInteract {
 
+	public ItemLibrary itemPool;
+	
 	public int max;
 	public int slots;
 	public List<Item> contents;
 	public bool isOpen;
 	public bool isLocked;
 
+	private void populateContainer() { 
+		int randomAmount = Random.Range(1, max);
+		Debug.Log($"randomAmount:{randomAmount}");
+	 contents= itemPool.PopulateContainer(randomAmount);
+	}
+
+	private void Awake() {
+		populateContainer();
+	}
 
 
 	public virtual void OnOpen() {
